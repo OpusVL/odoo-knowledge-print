6071bf950c7ca6058de546215708e92c80611ff3
Nick Booker <nick.booker@opusvl.com>
2014-10-16 12:04:10 +0100

    Initial commit.  Generated with odoogen-mod.

---
+++ document_page_print/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+# Document Page Printing
+# Copyright (C) 2014 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Document Page Printing',
+    'version': '0.1',
+    'author': 'OpusVL',
+    'website': 'http://opusvl.com/',
+    'summary': 'Export document pages for printing',
+    
+    'category': 'Knowledge',
+    
+    'description': """Export document pages for printing,
+""",
+    'images': [
+    ],
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
